078204641aa09f3cb9ed4f30e908dcda18382b1d
Henrique <hbaf71@gmail.com>
2021-10-11 10:29:06 -0300

    separando as funções em arquivos diferentes

---
+++ cores.js
@@ -0,0 +1,222 @@
+function coresTimes(time, jogo){
+	var dataCortada = jogo[5].split("-");
+	var ano = parseInt(dataCortada[0]);
+
+	switch (time) {
+		case "Água Santa":
+		return ["#153160", "white"];
+		break;
+		case "América-MG":
+		return ["#048439", "white"];
+		break;
+		case "América-RN":
+		return ["red", "white"];
+		break;
+		case "Athletico-PR":
+		return ["#c8060f", "black"];
+		break;
+		case "Atlético-GO":
+		return ["#ff0e00", "black"];
+		break
+		case "Atlético-MG":
+		return ["black", "white"];
+		break;
+		case "Atlético-PR":
+		return ["#ff0e00", "black"];
+		break;
+		case "Audax-SP":
+		return ["red", "white"];
+		break;
+		case "Avaí":
+		return ["#00679a", "white"];
+		break;
+		case "Bahia":
+		return ["#0188d6", "white"];
+		break;
+		case "Benfica-POR":
+		return ["red", "white"];
+		break;
+		case "Boca Juniors-ARG":
+		return ["#00458a", "#fcb000"];
+		break;
+		case "Botafogo":
+		return ["black", "white"];
+		break;
+		case "Botafogo-SP":
+		return ["red", "white"];
+		break;
+		case "Ceará":
+		return ["black", "white"];
+		break;
+		case "Cerro Porteño-PAR":
+		return ["#c40215", "#073756"];
+		break;
+		case "Chapecoense":
+		return ["#00913c", "white"];
+		break;
+		case "Corinthians":
+		return ["black", "white"];
+		break;
+		case "Coritiba":
+		return ["#00544d", "white"];
+		break;
+		case "Cruzeiro":
+		return ["#075798", "white"];
+		break;
+		case "CSA":
+		return ["#075798", "white"];
+		break;
+		case "Defensa y Justicia-ARG":
+		return ["#fde900", "#018036"];
+		break;
+		case "Deportivo Tachira-VEN":
+		return ["#f2d902", "black"];
+		break;
+		case "Estudiantes-ARG":
+		return ["red", "white"];
+		break;
+		case "Ferroviária":
+		return ["#511a1d", "white"];
+		break;
+		case "Figueirense":
+		return ["black", "white"];
+		break;
+		case "Flamengo":
+		return ["#ff0e00", "black"];
+		break;
+		case "Floresta-CE":
+		return ["#87c53f", "#036c3c"];
+		break;
+		case "Fluminense":
+		return ["#9f022f", "white"];
+		break;
+		case "Fortaleza":
+		return ["#11519b", "white"];
+		break;
+		case "Goiás":
+		if(ano < 2019){
+			return ["#048439", "white"];
+			break;
+		} else if (ano >= 2019){
+			return ["#006959", "white"];
+			break;
+		}
+		case "Grêmio":
+		return ["#0d80bf", "white"];;
+		break;
+		case "Grêmio Barueri":
+		return ["#f6e815", "#003c6e"];
+		break;
+		case "Guarani":
+		return ["#048439", "white"];
+		break;
+		case "Independiente-ARG":
+		return ["red", "white"];
+		break;
+		case "Independiente Santa Fé-COL":
+		return ["red", "white"];
+		break;
+		case "Internacional":
+		return ["#ff0e00", "white"];
+		break;
+		case "Ituano":
+		return ["red", "black"];
+		break;
+		case "Juventus":
+		return ["#6a023c", "white"];
+		break;
+		case "Kenitra-MAR":
+		return ["#038b5d", "white"];
+		break;
+		case "Linense":
+		return ["red", "white"];
+		break;
+		case "Luverdense":
+		return ["#8bc23d", "#426d33"];
+		break;
+		case "Mirassol":
+		return ["#ffec00", "#00722c"];
+		break;
+		case "Mogi Mirim":
+		return ["#ff0e00", "white"];
+		break;
+		case "Nacional-URU":
+		return ["#172b8c", "white"];
+		break;
+		case "Náutico":
+		return ["#ff0e00", "white"];
+		break;
+		case "Novorizontino":
+		return ["#fff500", "black"];
+		break;
+		case "Once Caldas-COL":
+		return ["black", "white"];
+		break;
+		case "Oeste":
+		return ["red", "black"];
+		break;
+		case "Palmeiras":
+		return ["#048439", "white"];
+		break;
+		case "Paraná":
+		return ["#2939b4", "white"];
+		break;
+		case "Paulista":
+		return ["red", "black"];
+		break;
+		case "Ponte Preta":
+		return ["black", "white"];
+		break;
+		case "Portuguesa":
+		return ["red", "#105d11"];
+		break;
+		case "Real Garcilaso-PER":
+		return ["#0197ee", "#fdf21c"];
+		break;
+		case "Red Bull Bragantino":
+		return ["#001c44", "white"];
+		break;
+		case "Red Bull Brasil":
+		return ["red", "white"];
+		break;
+		case "River Plate-URU":
+		return ["red", "white"];
+		break;
+		case "Santa Cruz":
+		return ["red", "white"];
+		break;
+		case "Santos":
+		return ["white", "black"];
+		break;
+		case "Santo André":
+		return ["#2d3491", "white"];
+		break;
+		case "São Bernardo":
+		return ["#ffdd00", "black"];
+		break;
+		case "São Bento":
+		return ["#0067ab", "white"];
+		break;
+		case "São Caetano":
+		return ["#212568", "white"];
+		break;
+		case "São Paulo":
+		return ["#ff0e00", "white"];
+		break;
+		case "Sport":
+		return ["#d30a11", "black"];
+		break;
+		case "Sporting Cristal-PER":
+		return ["#54c6ed", "white"];
+		break;
+		case "The Strongest-BOL":
+		return ["#ffcc00", "black"];
+		break;
+		case "Vasco":
+		return ["black", "white"];
+		break;
+		case "Vitória":
+		return ["#ff0e00", "black"];
+		break;
+	}
+}
---
+++ escreveLinha.js
@@ -0,0 +1,148 @@
+function cabecalho(){
+	var tabela = document.getElementById("jogos");
+	var qtdLinhas = tabela.rows.length;
+	var linha = tabela.insertRow(qtdLinhas);
+	tabela.width = "100%";
+	linha.width = "100%";
+	tabela.style.fontWeight = "bold";
+
+	// Célula número
+	var cellNumero = linha.insertCell(0);
+	cellNumero.innerHTML = "Número";
+	cellNumero.style.color = "black";
+	cellNumero.style.border = "1px solid black";
+
+	// Célula mandante
+	var cellMandante = linha.insertCell(1);
+	cellMandante.width = "40%";
+	cellMandante.innerHTML = "Mandante";
+	cellMandante.style.color = "black";
+	cellMandante.style.border = "1px solid black";
+
+	// Célula placar
+	var cellPlacar = linha.insertCell(2);
+	cellPlacar.width = "20%";
+	cellPlacar.innerHTML = "Placar";
+	cellPlacar.style.color = "black";
+	cellPlacar.style.border = "1px solid black";
+
+	// Célula visitante
+	var cellVisitante = linha.insertCell(3);
+	cellVisitante.width = "40%";
+	cellVisitante.innerHTML = "Visitante";
+	cellVisitante.style.color = "black";
+	cellVisitante.style.border = "1px solid black";
+}
+
+function escreveLinha(jogo, numero){
+	// setando o mandante e o visitante
+	var mandante;
+	var visitante;
+
+	if(jogo[0] == true){
+		mandante = "Santos";
+		visitante = jogo[1];
+	} else {
+		mandante = jogo[1];
+		visitante = "Santos";
+	}
+
+	// Pegando a tabela
+	var tabela = document.getElementById("jogos");
+	var qtdLinhas = tabela.rows.length;
+
+	// Célula com autores dos gols e técnico no meio 
+	var linha3 = tabela.insertRow(qtdLinhas);
+	linha3.id = "linha3";
+
+	var cellAutorMandante = linha3.insertCell(0);
+	cellAutorMandante.style.fontFamily = "Arial";
+	cellAutorMandante.style.fontWeight = "bold";
+	cellAutorMandante.style.backgroundColor = coresTimes(mandante, jogo)[0];
+	cellAutorMandante.style.color = coresTimes(mandante, jogo)[1];
+	cellAutorMandante.style.border = "1px solid black";
+	if(jogo[0] == true){
+		cellAutorMandante.innerHTML = jogo[8];
+	} else {
+		cellAutorMandante.innerHTML = jogo[9];
+	}
+
+	var cellTecnico = linha3.insertCell(1);
+	cellTecnico.style.fontFamily = "Arial";
+	cellTecnico.style.fontWeight = "bold";
+	cellTecnico.style.background = "linear-gradient(90deg, " + coresTimes(mandante, jogo)[0] + " 49%, " + coresTimes(visitante, jogo)[0] + " 52%)";
+	cellTecnico.style.textShadow = "0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000";
+	cellTecnico.style.border = "1px solid black";
+	cellTecnico.innerHTML = "Técnico: " + jogo[7];
+
+	var cellAutorVisitante = linha3.insertCell(2);
+	cellAutorVisitante.style.fontFamily = "Arial";
+	cellAutorVisitante.style.fontWeight = "bold";
+	cellAutorVisitante.style.backgroundColor = coresTimes(visitante, jogo)[0];
+	cellAutorVisitante.style.color = coresTimes(visitante, jogo)[1];
+	cellAutorVisitante.style.border = "1px solid black";
+	if(jogo[0] == true){
+		cellAutorVisitante.innerHTML = jogo[9];
+	} else {
+		cellAutorVisitante.innerHTML = jogo[8];
+	}
+
+	// Célula com times, escudos e placar
+	var linha2 = tabela.insertRow(qtdLinhas);
+	linha2.id = "linha2";
+
+	var cellMandante = linha2.insertCell(0);
+	cellMandante.id = "mandante" + numero;
+	cellMandante.style.fontFamily = "Arial";
+	cellMandante.style.fontWeight = "bold";
+	cellMandante.style.backgroundColor = coresTimes(mandante, jogo)[0];
+	cellMandante.style.color = coresTimes(mandante, jogo)[1];
+	cellMandante.style.border = "1px solid black";
+	cellMandante.innerHTML = "<img src=index_files/" + getEscudoName(mandante, jogo) + ".png width=70 height=70 alt=Imagem><br>" + mandante;
+
+	var cellPlacar = linha2.insertCell(1);
+	cellPlacar.id = "placar";
+	cellPlacar.style.fontFamily = "Arial";
+	cellPlacar.style.fontWeight = "bold";
+	cellPlacar.style.fontSize = "250%";
+	cellPlacar.style.whiteSpace = "nowrap";
+	cellPlacar.style.background = "linear-gradient(90deg, " + coresTimes(mandante, jogo)[0] + " 49%, " + coresTimes(visitante, jogo)[0] + " 52%)";
+	cellPlacar.style.textShadow = "0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000";
+	cellPlacar.style.border = "1px solid black";
+	if(jogo[0] == true){
+		cellPlacar.innerHTML = jogo[2] + " x " + jogo[3];
+	} else {
+		cellPlacar.innerHTML = jogo[3] + " x " + jogo[2];
+	}
+
+	var cellVisitante = linha2.insertCell(2);
+	cellVisitante.id = "visitante" + numero;
+	cellVisitante.style.fontFamily = "Arial";
+	cellVisitante.style.fontWeight = "bold";
+	cellVisitante.style.backgroundColor = coresTimes(visitante, jogo)[0];
+	cellVisitante.style.color = coresTimes(visitante, jogo)[1];
+	cellVisitante.style.border = "1px solid black";
+	cellVisitante.innerHTML = "<img src=index_files/" + getEscudoName(visitante, jogo) + ".png width=70 height=70 alt=Imagem><br>" + visitante;
+
+	// Célula com o número e informações
+	var linha1 = tabela.insertRow(qtdLinhas);
+	linha1.id = "linha1";
+
+	var cellNumero = linha1.insertCell(0);
+	cellNumero.rowSpan = 3;
+	cellNumero.style.fontFamily = "Arial";
+	cellNumero.style.fontWeight = "bold";
+	cellNumero.style.backgroundColor = coresTimes(mandante, jogo)[0];
+	cellNumero.style.color = coresTimes(mandante, jogo)[1];
+	cellNumero.style.border = "1px solid black";
+	cellNumero.innerHTML = numero; // Número do jogo
+
+	var cellCabecalho = linha1.insertCell(1);
+	cellCabecalho.colSpan = 3;
+	cellCabecalho.style.fontFamily = "Arial";
+	cellCabecalho.style.fontWeight = "bold";
+	cellCabecalho.style.background = "linear-gradient(90deg, " + coresTimes(mandante, jogo)[0] + " 49%, " + coresTimes(visitante, jogo)[0] + " 50%)";
+	cellCabecalho.style.textShadow = "0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000, 0 0 3px #000000";
+	cellCabecalho.style.border = "1px solid black";
+	cellCabecalho.innerHTML = converteData(jogo[5]) + " | " + "Estádio " + jogo[6] + " | " + jogo[4]; // Cabeçalho
+}

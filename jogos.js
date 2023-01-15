ed195e6d3930acd9731b958612ecf5a3013202bd
Henrique Borges De Almeida Filho <hbaf71@gmail.com>
2023-01-15 20:04:05 -0300

    santos 2 x 1 mirassol

--- jogos.js
+++ jogos.js
@@ -429,6 +429,8 @@ var jogo20230107 = ["Falcon", "Santos", 0, 2, "Copinha", "2023-01-07", "Bruno Jo
 jogos.push(jogo20230107);
 var jogo20230111 = ["Santo André", "Santos", 3, 0, "Copinha", "2023-01-11", "Bruno José Daniel", "Orlando Ribeiro", ["Gabriel Ferreira (2x)", "Bruninho"], [""], "https://www.youtube.com/embed/8MuTOQHwqek", false];
 jogos.push(jogo20230111);
+var jogo20230114 = ["Santos", "Mirassol", 2, 1, "Paulista", "2023-01-14", "Vila Belmiro", "Odair Hellmann", ["Marcos Leonardo", "Lucas Braga"], ["Danielzinho"], "https://www.youtube.com/embed/cCLrOqHRF64", false];
+jogos.push(jogo20230114);
 
 jogos.sort(function (a, b) {
     return a[5] < b[5] ? -1 : a[5] > b[5] ? 1 : 0;
